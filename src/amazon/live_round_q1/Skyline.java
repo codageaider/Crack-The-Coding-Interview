888ef9306f53a22d29dec6627d66465f38e6c85b
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-07-17 21:37:16 +0530

    toptal question

---
+++ src/amazon/live_round_q1/Skyline.java
@@ -0,0 +1,8 @@
+package amazon.live_round_q1;
+
+/*
+Question for recent Amazon virtual live coding round:
+https://github.com/codageaider/Crack-The-Coding-Interview/blob/main/src/amazon/live_round_q1/skyline-ladder-bricks.png
+ */
+public class Skyline {
+}
